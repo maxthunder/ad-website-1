5039a751c8c446033a1b95031c9ee43d48b44e04
maxthunder <maxwell.stark@gmail.com>
2020-05-30 02:33:31 -0600

    updates some animation

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -160,6 +160,16 @@
     items: 1
   });
 
+  // Temp image fallback to display when loading real image
+  window.addEventListener('load', function(){
+    var allimages= document.getElementsByTagName('img');
+    for (var i=0; i<allimages.length; i++) {
+      if (allimages[i].getAttribute('data-src')) {
+        allimages[i].setAttribute('src', allimages[i].getAttribute('data-src'));
+      }
+    }
+  }, false)
+
   // Initi AOS
   AOS.init({
     duration: 600
